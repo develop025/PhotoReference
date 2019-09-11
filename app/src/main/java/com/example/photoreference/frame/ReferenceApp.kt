fd81d88d0778d0e8776c3be4300af4aedc63e236
android <android@syneforge.com>
2019-09-11 14:04:09 +0300

    - Added stetho  - Added timber  - Fixed build

--- app/src/main/java/com/example/photoreference/frame/ReferenceApp.kt
+++ app/src/main/java/com/example/photoreference/frame/ReferenceApp.kt
@@ -1,21 +1,29 @@
 package com.example.photoreference.frame
 
 import android.app.Application
+import com.example.photoreference.BuildConfig
 import com.example.photoreference.di.referenceApp
+import com.example.photoreference.util.CrashReportingTree
 import com.facebook.stetho.Stetho
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class ReferenceApp : Application() {
 
     override fun onCreate() {
-        Stetho.initializeWithDefaults(this)
         super.onCreate()
         startKoin {
             androidLogger()
             androidContext(this@ReferenceApp)
             modules(referenceApp)
         }
+
+        if (BuildConfig.DEBUG) Stetho.initializeWithDefaults(this)
+
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        else Timber.plant(CrashReportingTree())
+
     }
 }
